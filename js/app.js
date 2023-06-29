1b0269c162fd66eed01069f8544bee64619ac331
Zhemchuzhnov Dmitriy <zhemchuzhnov.d@gmail.com>
2023-06-29 15:38:08 +0300

    Выделение активной ссылки

--- js/app.js
+++ js/app.js
@@ -6,4 +6,16 @@ $(document).ready(function () {
     fade: true,
     cssEase: "linear",
   });
+
+  // Выделение активной ссылки START
+  const address = window.location.pathname.split("/")[1].split(".")[0];
+  const menu_links = document.querySelectorAll(".menu_link");
+
+  menu_links.forEach((menu_link) => {
+    const page = menu_link.getAttribute("href").split("/")[1].split(".")[0];
+    if (page === address) {
+      menu_link.classList.add("active");
+    }
+  });
+  // Выделение активной ссылки END
 });
