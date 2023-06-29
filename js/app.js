11267d9a9a186cea7b98b28bc6ce23065ab2ed2d
Zhemchuzhnov Dmitriy <zhemchuzhnov.d@gmail.com>
2023-06-29 12:59:48 +0300

    Добавлен слайдер на страницу О работе

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".photo_case").slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+  });
+});
