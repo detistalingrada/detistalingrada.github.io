2e0b76ebaeb439670df252ec5468835886f15aae
Zhemchuzhnov Dmitriy <zhemchuzhnov.d@gmail.com>
2023-07-03 14:43:34 +0300

    Исправление с выделением пункта меню Главная

--- js/app.js
+++ js/app.js
@@ -11,11 +11,15 @@ $(document).ready(function () {
   const address = window.location.pathname.split("/")[1].split(".")[0];
   const menu_links = document.querySelectorAll(".menu_link");
 
-  menu_links.forEach((menu_link) => {
-    const page = menu_link.getAttribute("href").split("/")[1].split(".")[0];
-    if (page === address) {
-      menu_link.classList.add("active");
-    }
-  });
+  if (address === "") {
+    menu_links[0].classList.add("active");
+  } else {
+    menu_links.forEach((menu_link) => {
+      const page = menu_link.getAttribute("href").split("/")[1].split(".")[0];
+      if (page === address) {
+        menu_link.classList.add("active");
+      }
+    });
+  }
   // Выделение активной ссылки END
 });
